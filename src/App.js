ed273ecee04fbccaf63e6af44ddda03d1b7df4e8
johannfazakas <johannfazakas@gmail.com>
2020-12-20 09:16:30 +0200

    decision list grabbed from actual api

--- src/App.js
+++ src/App.js
@@ -2,21 +2,29 @@ import './App.css';
 import * as React from "react";
 import DecisionList from "./components/DecisionList";
 import CreateDecisionForm from "./components/CreateDecisionForm";
-
-const decisions = [
-  {name: 'bicicleta', description: 'cadou pentru parinti'},
-  {name: 'storcator fructe', description: 'cadou pentru buni'},
-  {name: 'rachete tenis', description: 'de la mama Crinei'}
-];
+import axios from "axios";
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {decisions: []};
+    axios.get("http://localhost:7733/dm-api/v1/decisions")
+      .then(response => {
+        this.setState(prevState => ({
+          decisions: response.data.items
+        }))
+      }, error => {
+        alert(`failed to grab decisions: ${error}`);
+      });
+  }
 
   render() {
     return (
       <div style={{backgroundColor: "darkolivegreen", padding: 50}}>
         <h1 style={{color: "whitesmoke"}}>Decision Maker Application</h1>
         <CreateDecisionForm />
-        <DecisionList decisions={decisions}/>
+        <DecisionList decisions={this.state.decisions} />
         <h3 style={{color: "whitesmoke"}}>footer</h3>
       </div>
     );
